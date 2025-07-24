49ab618940c0e2b5f463e56dea95443260076d17
Arturo <arturduar60@gmail.com>
2025-07-23 22:55:28 -0600

    Add item_task.xml

--- app/src/main/java/edu/udb/todo_app_android/AddTaskActivity.kt
+++ app/src/main/java/edu/udb/todo_app_android/AddTaskActivity.kt
@@ -1,12 +1,22 @@
 package edu.udb.todo_app_android
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class AddTaskActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_task)
+
+        val btnBack = findViewById<ImageView>(R.id.btn_back)
+        btnBack.setOnClickListener {
+            finish()
+        }
     }
+
+
 
 }
